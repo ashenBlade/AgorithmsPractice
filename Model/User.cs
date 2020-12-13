48b965cf011ef069ab11f4ead584a521d63e53f8
ashblade <htcdnsnnkom2002@gmail.com>
2020-12-13 14:11:37 +0300

    [Add] Basic user model
    User contains:
    - Nickname (ID)
    - Name (Real)
    - Surname (Real)
    - Birthdate
    - Gender

--- Model/User.cs
+++ Model/User.cs
@@ -2,5 +2,54 @@
 
 namespace WorkoutApp
 {
-    public class Class1 { }
+    public class User
+    {
+        /// <remarks>
+        /// Serves as identifier
+        /// Must be unique
+        /// </remarks>
+        public string Nickname { get; set; }
+
+        /// <summary>
+        /// Real user's name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Real user's surname
+        /// </summary>
+        public string Surname { get; set; }
+
+        /// <summary>
+        /// User's birthdate
+        /// </summary>
+        public DateTime? Birthdate { get; set; }
+
+        /// <summary>
+        /// User's gender
+        /// </summary>
+        public Gender Gender { get; set; }
+
+
+        /// <summary>
+        /// Unified constructor
+        /// </summary>
+        /// <param name="nickname"> Only necessary field</param>
+        /// <param name="name"> Real name </param>
+        /// <param name="surname"> Real surname </param>
+        /// <param name="birthdate"> Birthdate </param>
+        /// <param name="gender"> Gender </param>
+        public User(string nickname,
+                    string name = null,
+                    string surname = null,
+                    DateTime? birthdate = null,
+                    Gender gender = Gender.None)
+        {
+            Nickname = nickname;
+            Name = name;
+            Surname = surname;
+            Birthdate = birthdate;
+            Gender = gender;
+        }
+    }
 }
