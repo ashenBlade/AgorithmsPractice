a7261d476903b59e7d10edbc44fec14916668f57
ashblade <htcdnsnnkom2002@gmail.com>
2020-12-13 17:22:31 +0300

    [Add] Current user accessors
    Accessors for getting current user info

--- Controller/UserController.cs
+++ Controller/UserController.cs
@@ -1,9 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 
 namespace WorkoutApp.Controller
 {
-    public class User
+    public class UserController : BaseController
     {
+        /// <summary>
+        /// Current authorized user
+        /// </summary>
+        private User Current { get; set; }
 
+        #region CurrentUserInterface
+
+        public string GetNickname => Current.Nickname;
+        public string GetName => Current.Name;
+        public double? GetWeight => Current.Weight;
+        public double? GetHeight => Current.Height;
+        public DateTime? GetBirthdate => Current.Birthdate;
+        public Gender GetGender => Current.Gender;
+
+        #endregion
+
+        /// <summary>
+        /// All registered users
+        /// </summary>
+        /// <remarks>
+        /// Only in console version
+        /// </remarks>
+        private List<User> RegisteredUsers { get; set; }
+
+        public UserController()
+        {
+            // Load from database
+            RegisteredUsers = LoadUsers();
+            Current = null;
+        }
+
+        public void RegisterNewUser(string nickname)
+        {
+            var user = new User(nickname);
+            RegisteredUsers.Add(user);
+        }
+
+        public bool TryAuthorize(string nickname)
+        {
+            var user = RegisteredUsers.FirstOrDefault(i => i.Nickname == nickname);
+            Current = user ?? Current;
+            return Current != null;
+        }
+
+
+        private void Save() => Save(UserInfo.DataFile, RegisteredUsers);
+        private List<User> LoadUsers() => Load<List<User>>(UserInfo.DataFile) ?? new List<User>();
     }
 }
