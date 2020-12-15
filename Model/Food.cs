fdc3dc3c59d7fa26cee95c006c92eb736de17e2c
ashblade <htcdnsnnkom2002@gmail.com>
2020-12-15 17:54:45 +0300

    Add [Serializable] attribute

--- Model/Food.cs
+++ Model/Food.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace WorkoutApp
 {
+    [Serializable]
     public class Food
     {
         /// <summary>
