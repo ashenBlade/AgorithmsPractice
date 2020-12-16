81430b2d537b8b6a7a8d4845c2e4e6b14fc6f7da
ashblade <htcdnsnnkom2002@gmail.com>
2020-12-16 08:08:03 +0300

    Add attribute for user's nickname validation

---
+++ Model/Attributes/NicknameAttribute.cs
@@ -0,0 +1,32 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace WorkoutApp.Attributes
+{
+    /// <summary>
+    /// Asserts that nickname contains only possible characters
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class NicknameAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (Regex.IsMatch(value.ToString() ?? string.Empty,@"\s"))
+            {
+                return new ValidationResult("Nickname can not contain whitespaces");
+            }
+            var nicknamePattern = @"^[\w*[A-Za-z0-9]+\w*]*";
+            if (Regex.Match(value.ToString() ?? string.Empty, nicknamePattern).Success)
+                    return ValidationResult.Success;
+            return new
+                ValidationResult("Nickname can contain only letters, digits and underline character");
+        }
+
+        public override bool IsValid(object value)
+        {
+            return Regex.Match(value.ToString() ?? string.Empty, @"\s").Success &&
+                Regex.Match(value.ToString() ?? string.Empty, @"^[\w*[A-Za-z0-9]+\w*]*").Success;
+        }
+    }
+}
