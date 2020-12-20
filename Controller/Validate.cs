8384be000e7af0fd5bd3ed984f202ab9fbc6a811
ashblade <htcdnsnnkom2002@gmail.com>
2020-12-20 11:42:59 +0300

    Add static class for values validation

---
+++ Controller/Validate.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using WorkoutApp.Attributes;
+
+namespace WorkoutApp.Controller
+{
+    public static class Validate
+    {
+        public static List<ValidationResult> UserName(string name)
+        {
+            var validationContext = new ValidationContext(name);
+            var results = new List<ValidationResult>();
+            Validator.TryValidateValue(name, validationContext, results, new []{ new UserNameAttribute() });
+            return results;
+        }
+    }
+}
